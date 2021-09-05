1c55519edb09fc633cd170d4dba4b1a27b65d8f1
matheusnalmeida <matheusnalmeida20@gmail.com>
2021-09-05 13:30:23 -0300

    Removendo pasta DTO da camada de dominio e testes unitarios para telefone

---
+++ Backend/AutoShop.Test/ValueObjects/TelefoneTest.cs
@@ -0,0 +1,44 @@
+﻿using AutoShop.Domain.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoShop.Test.ValueObjects
+{
+    [TestClass]
+    public class TelefoneTest
+    {
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("(68) 70127-3739")]
+        [DataRow("682222222")]
+        [DataRow("68202222222")]
+        public void ShouldReturnErrorWhenTelefoneIsInvalid(string telefone)
+        {
+            var telefoneVo = new Telefone(telefone);
+            Assert.IsFalse(telefoneVo.IsValid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenTelefoneIsNull()
+        {
+            var telefone = new Telefone(null);
+            Assert.IsFalse(telefone.IsValid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("4827886506")]
+        [DataRow("48981371600")]
+        [DataRow("6929862104")]
+        public void ShouldReturnSucessWhenTelefoneIsValid(string telefone)
+        {
+            var telefoneVo = new Telefone(telefone);
+            Assert.IsTrue(telefoneVo.IsValid);
+        }
+    }
+}
